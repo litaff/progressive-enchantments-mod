002c0befc063f184eb59bf1cd869dcbc59f339d1
litaff <a.gazda2k18@gmail.com>
2025-02-26 15:05:08 +0100

    Feature/bumped to 1.21.4 + fresh begining

--- src/main/java/progressive_enchantments/ProgressiveEnchantments.java
+++ src/main/java/progressive_enchantments/ProgressiveEnchantments.java
@@ -1,14 +1,17 @@
-package net.fabricmc.progressive_enchantments;
+package progressive_enchantments;
 
 import net.fabricmc.api.ModInitializer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProgressiveEnchantments implements ModInitializer {
+	public static final String MOD_ID = "progressive-enchantments";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("progressive_enchantments");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +19,6 @@ public class ProgressiveEnchantments implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Progressive Enchantments mod loaded");
+		LOGGER.info("Hello Fabric world!");
 	}
 }
